b35ccc4d410eccc17a331a15bbc3a8254960ed31
3788-lacarrillo6 <118786400+3788-lacarrillo6@users.noreply.github.com>
2025-03-10 18:27:39 -0500

    sd

--- ProyectoParcial3/index.php
+++ ProyectoParcial3/index.php
@@ -10,7 +10,8 @@
 
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
   </head>
-  <body style="background-color: blue;">
+  <body style="background-color:rgb(207, 190, 33);">
+
 
 
 
